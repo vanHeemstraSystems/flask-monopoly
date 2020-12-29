340dede869f7816b82949143cf3ef83121b1802e
krzysztofwelc <krzysiekwelc18@gmail.com>
2020-12-29 17:05:26 +0100

    saving and loading games as an util

--- app/game/utils.py
+++ app/game/utils.py
@@ -1,5 +1,28 @@
 import os
+import pickle
+from typing import Union
+from app.game.game import Game
 
 
 def get_games_dir():
     return os.path.abspath(os.getcwd()) + '/app/game/games'
+
+
+def save_game(game: Game, code: str) -> str:
+    filename = get_games_dir() + '/{}.pkl'.format(code)
+
+    with open(filename, 'wb') as f:
+        pickle.dump(game, f, pickle.HIGHEST_PROTOCOL)
+
+    return code
+
+
+def load_game(code: str) -> Union[Game, None]:
+    filename = get_games_dir() + '/{}.pkl'.format(code)
+    try:
+        f = open(filename, 'rb')
+        g = pickle.load(f)
+        f.close()
+        return g
+    except FileNotFoundError:
+        return None
