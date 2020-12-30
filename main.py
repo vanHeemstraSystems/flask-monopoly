b835fc612c20d167301d89a31584b3cd73193229
krzysztofwelc <krzysiekwelc18@gmail.com>
2020-12-30 12:08:53 +0100

    script for removing save files added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from app import create_app, db
+from app.game.utils import get_games_dir
 
 app = create_app()
 
@@ -6,6 +8,15 @@ app = create_app()
 @app.cli.command('create_db')
 def create_db():
     db.create_all()
+
+
+@app.cli.command('clear_saves')
+def clear_saves():
+    files = os.listdir(get_games_dir())
+    for f in files:
+        _, ext = os.path.splitext(f)
+        if ext == '.pkl':
+            os.remove(get_games_dir()+'/'+f)
 
 
 if __name__ == '__main__':
