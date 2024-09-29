ac4ad0ca7cbf8c0193ca29d32c13d44d391981c5
Willem van Heemstra <wvanheemstra@icloud.com>
2024-09-29 12:26:44 +0000

    Update

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from flask import Flask
+from flask_assets import Environment, Bundle
 
 def create_app():
     app = Flask(__name__)
     app_settings = 'app.config.Config'
     app.config.from_object(app_settings)
-
+    assets = Environment(app)
+    assets.url = app.static_url_path
+    scss = Bundle('scss/style.scss', 'scss/tiles.scss', filters='pyscss', output='css/all.css')
+    assets.register('scss_all', scss)
     # MORE
 
     return app
