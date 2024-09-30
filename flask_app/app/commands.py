e9026be335e83554321da8eb00f83153173fb004
Willem van Heemstra <wvanheemstra@icloud.com>
2024-09-30 09:24:12 +0000

    Update

--- flask_app/app/commands.py
+++ flask_app/app/commands.py
@@ -1,4 +1,5 @@
 import click
+from app import db
 from flask import Blueprint
 
 commands = Blueprint('commands', __name__)
@@ -6,4 +7,5 @@ commands = Blueprint('commands', __name__)
 @commands.cli.command('create_db')
 def create_db():
     """ Creates a database """
+    db.create_all()
     print("Database created")
